2a8d9e20b947ff4f4cc78e8466b57fd659096906
Andy <anderzanovoleg@gmail.com>
2022-07-18 12:47:05 +0300

    server structure

---
+++ api.py
@@ -0,0 +1,25 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:63343",
+    "http://localhost:3000",
+    "http://127.0.0.1:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=7000)
